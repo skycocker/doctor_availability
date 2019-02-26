014816d00662fb2deaa4354a284bfa6e84b07aa8
Michał Siwek <mike21@aol.pl>
2019-02-26 19:54:16 +0100

    Clean up some syntax a little

--- lib/doctor_availability/errors.rb
+++ lib/doctor_availability/errors.rb
@@ -1,7 +1,7 @@
 module DoctorAvailability
   class NoSuchDayError < StandardError
     def initialize(input_weekday)
-      STDERR.puts "there is no such day as #{input_weekday}, check your spelling and try again"
+      STDERR.puts("there is no such day as #{input_weekday}, check your spelling and try again")
       super(input_weekday)
     end
   end
